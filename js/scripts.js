668c287bc4526e487da423b2c6b3c1e3995f1b5a
Evan Clough & Aileen Cacayorin <student@epicodus.com>
2015-09-10 15:27:54 -0700

    Write logic to count words in string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
-var wordCount = function(year) {
+var wordCount = function(text) {
+  var arr = text.replace(/\W+/g, " ").trim().split(' ');
+  var counts = {};
 
-  // stuff
+  for(var i = 0; i< arr.length; i++) {
+      var word = arr[i];
+      counts[word] = counts[word] ? counts[word]+1 : 1;
+  }
 
+  return counts;
 };
 
 $(document).ready(function(){
